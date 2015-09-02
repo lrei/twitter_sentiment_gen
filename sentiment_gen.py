6063f2fc6a8e4c6bddb5de8f4c42f2c7afc7e021
mbizjak <matic.bizjak94@gmail.com>
2015-09-02 13:58:19 +0200

    added final txt files, queue sizes correspond with those in other main methods

--- sentiment_gen.py
+++ sentiment_gen.py
@@ -27,6 +27,8 @@ import os
 import json
 import argparse
 from functools import partial
+import codecs
+import gzip
 
 from newsfeed_tweets import convert_tweets
 from filter_lang import filter_line
@@ -36,33 +38,33 @@ from classify_langid import filter_classify_lang_line
 from MultiprocessFiles import MultiprocessFiles
 import twokenize
 from tokenize import word_tokenize, tokenize_tweet
+from filter_emoticons import process_line
 
 
 def main():
     """ main """
-    # default parameters
-    min_tokens = 10
-    max_num_urls = 1
-    max_num_users = 3
-    newsfeed_path = None
-    langid_min_prob = 0.8
     replacements = json.load(open('replacements.json'))
+    # Stats Vars
+    n_pos = 0
+    n_neg = 0
+    n = 0
+    POS = True
+    NEG = False
 
     parser = argparse.ArgumentParser()
 
     # Basic
     parser.add_argument('tweets_file')
-    parser.add_argument('-p', '--news_feed_path')
+    parser.add_argument('-p', '--news_feed_path', default=None)
     parser.add_argument('lang_codes', help='lang codes comma-seperated')
     
     # Preprocessing Part I
-    parser.add_argument('-t', '--min_tokens', type=int)
-    parser.add_argument('-r', '--max_urls', type=int)
-    parser.add_argument('-u', '--max_users', type=int)
+    parser.add_argument('-t', '--min_tokens', type=int, default=5)
+    parser.add_argument('-r', '--max_urls', type=int, default=2)
+    parser.add_argument('-u', '--max_users', type=int, default=3)
 
     # Language Identification
-    parser.add_argument('min_langid_prob', type=float)
-    parser.add_argument('-prob', '--langid_min_prob')
+    parser.add_argument('-prob', '--langid_min_prob', type=float, default=0.8)
 
     # Tokenization
     parser.add_argument('-s', '--simple', dest='simple', action='store_true',
@@ -76,28 +78,25 @@ def main():
  
     # Preprocessing Part II
     parser.add_argument('-l', '--lowercase', dest='lowercase',
-                        action='store_true')
+                        action='store_true', default=False)
     parser.add_argument('-b', '--break_hash', dest='break_hash',
                         action='store_true', default=False)
 
     # Sentiment Dataset Generation
-    parser.add_argument('prob_smiley', type=float)
+    parser.add_argument('-m', '--prob_smiley', type=float, default=0.4)
+    parser.add_argument('-o', '--output_dir', default=None)
 
     args = parser.parse_args()
 
     lang_codes = unicode(args.lang_codes).split(',')
     prob_smiley = args.prob_smiley
     tweets_file = args.tweets_file
-    if args.news_feed_path:
-        newsfeed_path = args.news_feed_path
-    if args.min_tokens:
-        min_tokens = args.min_tokens
-    if args.max_urls:
-        max_num_urls = args.max_urls
-    if args.max_users:
-        max_num_users = args.max_users
-    if args.langid_min_prob:
-        langid_min_prob = args.langid_min_prob
+    newsfeed_path = args.news_feed_path
+    min_tokens = args.min_tokens
+    max_num_urls = args.max_urls
+    max_num_users = args.max_users
+    langid_min_prob = args.langid_min_prob
+    output_directory = args.output_dir
     tokenize_function = twokenize.tokenize2
     if args.simple:
         tokenize_function = word_tokenize
@@ -128,7 +127,7 @@ def main():
         outfile = os.path.join(tweets_path, outfile)
         multiprocess_filter_lang = MultiprocessFiles(tweets_file, outfile, func,
                                                      num_procs=0,
-                                                     queue_size=200000)
+                                                     queue_size=3000)
         multiprocess_filter_lang.run()
 
         # Preprocess Text
@@ -138,19 +137,19 @@ def main():
         func = partial(preprocess_tweet, min_tokens,
                        max_num_urls, max_num_users, replacements)
         preprocess = MultiprocessFiles(input_file, output_file, func,
-                                       num_procs=0, queue_size=200000)
+                                       num_procs=0, queue_size=2000)
         preprocess.run()
 
+        # Lang Identification
         tweet_file = output_file
         dest_file = 'tweets.' + lang_code + '.pp.lid.json.gz'
         dest_file = os.path.join(tweets_path, dest_file)
 
         func = partial(filter_classify_lang_line, lang_code, langid_min_prob,
                        replacements)
-
-        # Lang Identification
+        
         classify = MultiprocessFiles(tweet_file, dest_file, func, num_procs=0,
-                                     queue_size=200000)
+                                     queue_size=2000)
         classify.run()
 
         # Preprocess 2
@@ -161,7 +160,7 @@ def main():
             pp2_func = partial(preprocess2, args.lowercase, args.break_hash,
                                replacements)
             pp2 = MultiprocessFiles(infile, outfile, pp2_func,
-                                    num_procs=0, queue_size=200000)
+                                    num_procs=0, queue_size=2000)
             pp2.run()
 
         # Tokenization
@@ -174,10 +173,46 @@ def main():
 
         func = partial(tokenize_tweet, tokenize_function)
         tokenizer = MultiprocessFiles(source, dest, func, num_procs=0,
-                                      queue_size=200000)
+                                      queue_size=2000)
         tokenizer.run()
 
-    # @todo remove tmpdir and result should be outside tmpdir
+        # @todo remove tmpdir and results should be outside tmpdir
+
+        # open output files
+        if output_directory:
+            tweets_path = output_directory
+
+        pos_path = os.path.join(tweets_path, 'pos.txt')
+        f_pos = codecs.open(pos_path, 'w', encoding='utf-8')
+
+        neg_path = os.path.join(tweets_path, 'neg.txt')
+        f_neg = codecs.open(neg_path, 'w', encoding='utf-8')
+
+        other_path = os.path.join(tweets_path, 'other.txt')
+        f_other = codecs.open(other_path, 'w', encoding='utf-8')
+
+        # Read and Process
+        with gzip.open(dest, 'r') as f:
+            for line in f:
+                res = process_line(prob_smiley, line)
+                n += 1
+
+                if res is None:
+                    continue
+                if res[0] is None:
+                    f_other.write(res[1] + u'\n')
+                if res[0] == POS:
+                    n_pos += 1
+                    f_pos.write(res[1] + u'\n')
+                if res[0] == NEG:
+                    f_neg.write(res[1] + u'\n')
+                    n_neg += 1
+
+        # explicitly close files
+        f_pos.close()
+        f_neg.close()
+        f_other.close()
+
 
 if __name__ == '__main__':
     main()
