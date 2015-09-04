86c47d612b6eef23262ad87442bbd6697bf96928
mbizjak <matic.bizjak94@gmail.com>
2015-09-04 13:50:17 +0200

    new script filter_unique

--- filter_unique.py
+++ filter_unique.py
@@ -1,1 +1,42 @@
-__author__ = 'mbizjak'
+import argparse
+import gzip
+import json
+
+
+def make_unique(infile, outfile, tweet_ids):
+    fout = gzip.open(outfile, 'w')
+    with gzip.open(infile, 'r') as source:
+        for line in source:
+            try:
+                tweet = json.loads(line)
+            except:
+                continue
+
+            if tweet['id'] in tweet_ids:
+                continue
+
+            if 'retweet_id' in tweet and tweet['retweet_id'] in tweet_ids:
+                continue
+
+            tweet_ids.add(tweet['id'])
+            if 'retweet_id' in tweet:
+                tweet_ids.add(tweet['retweet_id'])
+
+            tweet_string = json.dumps(tweet) + '\n'
+            fout.write(tweet_string)
+
+    fout.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+    args = parser.parse_args()
+    tweet_ids = set()
+    make_unique(args.infile, args.outfile, tweet_ids)
+
+
+if __name__ == '__main__':
+    main()
